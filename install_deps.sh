3764f3c47ffcfb251d2aab4147a2490be13bcb56
Ian Stark <Ian.Stark@ed.ac.uk>
2014-10-14 13:43:59 +0100

    Revise install script to pin enough packages that it builds today

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
-sudo  apt-get -y update
-sudo  apt-get -y install ghc libncurses5-dev happy alex libcairo2-dev libpango1.0-dev gtk+-3.0 glib-2.0 gsl-bin gsl-ref-html libgsl0-dev liblapack-dev libgtksourceview-3.0 libgtksourceview-3.0-dev cabal-install
+sudo apt-get -y update
+sudo apt-get -y install gnome-platform haskell-platform
+sudo apt-get -y install libncurses5-dev libgsl0-dev liblapack-dev
 cabal update
-sudo cabal install --global gtk2hs-buildtools text hledger gtk3 MissingH hmatrix data-accessor colour Chart Chart-cairo hmatrix-gsl gtksourceview3
+cabal install 'gtk2hs-buildtools==0.12.*' 
+export PATH=${PATH}:${HOME}/.cabal/bin
+cabal install 'gtksourceview3==0.12.*'
+cabal install text hledger MissingH hmatrix-gsl data-accessor
+cabal install 'Chart==1.2' 'Chart-cairo==1.2.*'
+
+# Now run "make" to build cpi-ide
